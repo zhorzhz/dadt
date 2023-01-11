eb25c77eacac3a43b5e4ac673b829a23fc73b369
Zhorzh <zhorzh.zakaryan@gmail.com>
2023-01-12 00:26:33 +0100

    fixed bug

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var DB = require('./db/connect')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // Add static services from node modules static
 const statics = ['bootstrap', 'jquery', 'chart.js'];
